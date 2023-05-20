b0d1a20b932e316fb7bbb80df612e61387f0374d
hekto2004@mail.ru <235113Qwe>
2023-05-21 01:29:02 +0700

    тест гитхаба

---
+++ app/src/main/java/com/example/handbook/test2.java
@@ -0,0 +1,13 @@
+package com.example.handbook;
+
+public class test2 {
+    String s = "hi";
+    public void testing (String st){
+        this.s=st;
+    }
+
+    public void  sayHello(String s ){
+        System.out.println(s);
+
+    }
+}
